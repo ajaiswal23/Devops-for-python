16490c6aa812e7117ee4742658ce8bc1b0110242
ajaiswal23 <addyjaiswal2309@gmail.com>
2022-10-24 18:01:08 -0700

    added format in makefile

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ lint:
 
 test:
 	python -m pytest -vv --cov=devopslib test_*.py
+
+format:
+	black *py devopslib/*.py
--- test_devopslib.py
+++ test_devopslib.py
@@ -1,5 +1,6 @@
 from devopslib.random_fruit import fruit
 
+
 def test_fruit():
     fruit_choice = fruit()
     assert fruit_choice in ["apple", "cherry", "strawberry", "mango"]
